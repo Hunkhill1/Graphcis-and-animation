ac5db9d10822b203451f0779d7bcb226437cf4db
Sahil Narula <126839546+Hunkhill1@users.noreply.github.com>
2024-04-23 17:41:54 +0800

    added and downloaded dependencies using cmake with given code

---
+++ CITS3003/Labs/lab_01/cits3003_lab6/cmake-build-debug/CMakeFiles/cits3003_lab6.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/sahil/Graphcis-and-animation/CITS3003/Labs/lab_01/cits3003_lab6/helpers/ShaderHelper.cpp" "CMakeFiles/cits3003_lab6.dir/helpers/ShaderHelper.cpp.o" "gcc" "CMakeFiles/cits3003_lab6.dir/helpers/ShaderHelper.cpp.o.d"
+  "/home/sahil/Graphcis-and-animation/CITS3003/Labs/lab_01/cits3003_lab6/helpers/imgui/ImGuiImpl.cpp" "CMakeFiles/cits3003_lab6.dir/helpers/imgui/ImGuiImpl.cpp.o" "gcc" "CMakeFiles/cits3003_lab6.dir/helpers/imgui/ImGuiImpl.cpp.o.d"
+  "/home/sahil/Graphcis-and-animation/CITS3003/Labs/lab_01/cits3003_lab6/helpers/imgui/ImGuiManager.cpp" "CMakeFiles/cits3003_lab6.dir/helpers/imgui/ImGuiManager.cpp.o" "gcc" "CMakeFiles/cits3003_lab6.dir/helpers/imgui/ImGuiManager.cpp.o.d"
+  "/home/sahil/Graphcis-and-animation/CITS3003/Labs/lab_01/cits3003_lab6/main.cpp" "CMakeFiles/cits3003_lab6.dir/main.cpp.o" "gcc" "CMakeFiles/cits3003_lab6.dir/main.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CITS3003/Labs/lab_01/cits3003_lab6/cmake-build-debug/CMakeFiles/cits3003_lab6.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cits3003_lab6.dir/helpers/ShaderHelper.cpp.o"
+  "CMakeFiles/cits3003_lab6.dir/helpers/ShaderHelper.cpp.o.d"
+  "CMakeFiles/cits3003_lab6.dir/helpers/imgui/ImGuiImpl.cpp.o"
+  "CMakeFiles/cits3003_lab6.dir/helpers/imgui/ImGuiImpl.cpp.o.d"
+  "CMakeFiles/cits3003_lab6.dir/helpers/imgui/ImGuiManager.cpp.o"
+  "CMakeFiles/cits3003_lab6.dir/helpers/imgui/ImGuiManager.cpp.o.d"
+  "CMakeFiles/cits3003_lab6.dir/main.cpp.o"
+  "CMakeFiles/cits3003_lab6.dir/main.cpp.o.d"
+  "cits3003_lab6"
+  "cits3003_lab6.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cits3003_lab6.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
