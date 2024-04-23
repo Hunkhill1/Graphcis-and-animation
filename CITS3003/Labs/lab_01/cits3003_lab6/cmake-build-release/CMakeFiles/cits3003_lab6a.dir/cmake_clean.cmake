e57ee2c8eafc87d1a65050017b7f442664272f80
Sahil Narula <126839546+Hunkhill1@users.noreply.github.com>
2024-04-23 18:47:38 +0800

    Finshed q1

---
+++ CITS3003/Labs/lab_01/cits3003_lab6/cmake-build-release/CMakeFiles/cits3003_lab6a.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cits3003_lab6a.dir/helpers/ShaderHelper.cpp.o"
+  "CMakeFiles/cits3003_lab6a.dir/helpers/ShaderHelper.cpp.o.d"
+  "CMakeFiles/cits3003_lab6a.dir/helpers/imgui/ImGuiImpl.cpp.o"
+  "CMakeFiles/cits3003_lab6a.dir/helpers/imgui/ImGuiImpl.cpp.o.d"
+  "CMakeFiles/cits3003_lab6a.dir/helpers/imgui/ImGuiManager.cpp.o"
+  "CMakeFiles/cits3003_lab6a.dir/helpers/imgui/ImGuiManager.cpp.o.d"
+  "CMakeFiles/cits3003_lab6a.dir/q1amat4.cpp.o"
+  "CMakeFiles/cits3003_lab6a.dir/q1amat4.cpp.o.d"
+  "cits3003_lab6a"
+  "cits3003_lab6a.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cits3003_lab6a.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ CITS3003/Labs/lab_01/cits3003_lab6/cmake-build-release/CMakeFiles/cits3003_lab6a.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for cits3003_lab6a.
+# This may be replaced when dependencies are built.
